655881a31364b6a6888c8c090fbaa6c664daee82
j721 <juliadvu@gmail.com>
2020-04-27 12:32:54 -0500

    worked on Login component and imported into app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }

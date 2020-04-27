cc9ccf7433984d93f339569b68e3172aa6b823a6
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-27 14:21:04 -0700

    login / register routes updated, login / register form validation done

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import * as yup from 'yup'
 
 
 const initialState = {
@@ -11,25 +11,87 @@ const initialState = {
 
 }
 
+const initialFormErrors = {
+
+
+    username:'Username is required!',
+    password:'Password is required!',
+
+}
+
+const loginSchema = yup.object().shape({
+
+    username: yup
+        .string()
+        .min(3, 'Username must include at least 3 characters!')
+        .required('Username is required!'),
+    password: yup
+        .string()
+        .min(5, 'Password must include at least 5 characters!')
+        .required('Password is required!')    
+
+});
+
 export const Login = (props)=>{
 
     const [login, setLogin] = useState(initialState);
+    const [loginFormErrors, setLoginFormErrors] = useState(initialFormErrors)
+    const [buttonEnabled, setButtonEnabled] = useState(false)
+
+
+    useEffect(() => {
+
+
+        loginSchema.isValid(login)
+            .then(valid => {
+
+
+                setButtonEnabled(valid)
+
+
+            })
+
+
+
+    }, [login])
+
+
+
+
+    
+    
 
     const handleChange = (e) => {
 
+        e.persist()
         setLogin({...login, [e.target.name]:e.target.value})
+
+        yup
+            .reach(loginSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+
+                setLoginFormErrors({...loginFormErrors, [e.target.name]:''})
+
+            })
+            .catch(err => {
+
+                setLoginFormErrors({...loginFormErrors, [e.target.name]:err.errors[0]})
+
+            })
 
     }
 
     
     const handleSubmit = (e) => {
 
+        
         e.preventDefault();
         axios.post('', login)
             .then(res=> {
 
                 console.log('Login data returning', res)
-                props.history.push('');
+                props.history.push('/');
 
             })
             .catch(err=> {
@@ -53,8 +115,9 @@ export const Login = (props)=>{
 
                 <label>Password:</label><input placeholder='password' onChange={handleChange} type='password' name='password' value={login.password}></input>
 
-                <button type='submit'>Login</button>
-
+                <button disabled={!buttonEnabled} type='submit'>Login</button>
+                <p>{loginFormErrors.username}</p>
+                <p>{loginFormErrors.password}</p>
                 {login.isFetching && 'Loading login page...'}
 
             </form>

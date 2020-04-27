b67c8f9cf06bbfb235f9cd9f306324e8b52fc29c
j721 <juliadvu@gmail.com>
2020-04-27 17:15:10 -0500

    pulled login and register components from patrick's branch with yup validation added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Route, useHistory } from "react-router-dom"
 import { Login } from "./components/Login";
+import Register from "./components/Register"
+
+
+
 
 function App() {
+
+  let history = useHistory();
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      
+      <Route path='/register'>
+        <Register history={history}/>
+      </Route>
+      <Route path='/login'>
+        <Login history={history}/>
+      </Route>
+      
     </div>
   );
 }

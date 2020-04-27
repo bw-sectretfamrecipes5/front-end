5147f6f6fb7e2b52d0475f3399f82053a2cc9889
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-27 10:47:55 -0700

    created change and submit functions for forms

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const initialState = {
+
+    username:'',
+    password:'',
+    isFetching:false
+
+}
 
 export const Login = ()=>{
+
+    const [login, setLogin] = useState(initialState);
+
+    const handleChange = (e) => {
+
+        setLogin({...login, [e.target.name]:e.target.value})
+
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        axios.post('', login)
+            .then(res=> {
+
+                console.log('Login data returning', res)
+
+            })
+            .catch(err=> {
+
+                console.log('Login data failed to return', err)
+
+            })
+
+    }
+
+
+
+
     return (
         <div></div>
     )

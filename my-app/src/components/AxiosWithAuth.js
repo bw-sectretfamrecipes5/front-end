e76b3502b5db137248f3392f779ae4eb9bbd9fa6
j721 <juliadvu@gmail.com>
2020-04-28 12:22:33 -0500

    changes to axiosWithAuth

--- my-app/src/components/AxiosWithAuth.js
+++ my-app/src/components/AxiosWithAuth.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token"); 
   return axios.create({
-    baseURL: "https://secret-family-recipes-bw-team5.herokuapp.com/",
+    baseURL: "https://us04web.zoom.us/j/79903809883?pwd=akx4cmkraHoybm5JcGQrN05Dd0dqdz09",
     headers: {
       Authorization: token
     }
   });
 };
+
+console.log(baseURL);
 

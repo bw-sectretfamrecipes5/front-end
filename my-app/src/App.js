c980d5a5225889f3e2fd15a60dff8532da563b3e
Danial Hadavi <danialhadavi@gmail.com>
2020-04-28 13:10:25 -0400

    working on bugs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Route, useHistory, Switch } from "react-router-dom"
+import { Route, useHistory, Switch } from "react-router-dom";
 import { Login } from "./components/Login";
-import Register from "./components/Register"
-
-
-
+import Register from "./components/Register";
 
 function App() {
-
   let history = useHistory();
 
   return (
     <div className="App">
       <Switch>
-        <Route path='/register'>
-          <Register history={history}/>
+        <Route path="/register">
+          <Register history={history} />
         </Route>
-        <Route path='/login'>
-          <Login history={history}/>
+        <Route path="/login">
+          <Login history={history} />
         </Route>
       </Switch>
-      
     </div>
   );
 }
--- my-app/src/components/Register.js
+++ my-app/src/components/Register.js
@@ -1,133 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import * as yup from 'yup'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
 
 const initialState = {
-
-    username: '',
-    password: '',
-    email: '',
-    isFetching: false
-
-}
+  username: "",
+  password: "",
+  email: "",
+  isFetching: false,
+};
 const initialFormErrors = {
-
-
-    username:'Username is required!',
-    password:'Password is required!',
-    email:'Email is required!'
-
-}
+  username: "Username is required!",
+  password: "Password is required!",
+  email: "Email is required!",
+};
 
 const registerSchema = yup.object().shape({
-
-    username: yup
-        .string()
-        .min(3, 'Username must include at least 3 characters!')
-        .required('Username is required!'),
-    password: yup
-        .string()
-        .min(5, 'Password must include at least 5 characters!')
-        .required('Password is required!') ,
-    email: yup
-        .string()
-        .email('Must be a valid email address!')
-        .required('Email is required!')  
-
+  username: yup
+    .string()
+    .min(3, "Username must include at least 3 characters!")
+    .required("Username is required!"),
+  password: yup
+    .string()
+    .min(5, "Password must include at least 5 characters!")
+    .required("Password is required!"),
+  email: yup
+    .string()
+    .email("Must be a valid email address!")
+    .required("Email is required!"),
 });
 
 function Register(props) {
+  const [register, setRegister] = useState(initialState);
+  const [registerFormErrors, setRegisterFormErrors] = useState(
+    initialFormErrors
+  );
+  const [buttonEnabled, setButtonEnabled] = useState(false);
 
-    const [register, setRegister] = useState(initialState)
-    const [registerFormErrors, setRegisterFormErrors] = useState(initialFormErrors)
-    const [buttonEnabled, setButtonEnabled] = useState(false)
+  useEffect(() => {
+    registerSchema.isValid(register).then((valid) => {
+      setButtonEnabled(valid);
+    });
+  }, [register]);
 
-    useEffect(() => {
+  const handleChange = (e) => {
+    e.persist();
+    setRegister({ ...register, [e.target.name]: e.target.value });
 
-        registerSchema.isValid(register)
-            .then(valid => {
+    yup
+      .reach(registerSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setRegisterFormErrors({ ...registerFormErrors, [e.target.name]: "" });
+      })
+      .catch((err) => {
+        setRegisterFormErrors({
+          ...registerFormErrors,
+          [e.target.name]: err.errors[0],
+        });
+      });
+  };
 
-                setButtonEnabled(valid)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-            })
-            
+    axios
+      .post("", register)
+      .then((res) => {
+        props.history.push("/");
+        console.log("Register data is returning", res);
+      })
+      .catch((err) => {
+        console.log("Register data is not returning", err);
+      });
+  };
 
+  return (
+    <div className="register">
+      <h1>Register</h1>
 
-    }, [register])
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input
+          placeholder="username"
+          onChange={handleChange}
+          type="text"
+          name="username"
+          value={register.username}
+        ></input>
 
-    const handleChange = (e) => {
+        <label>Password:</label>
+        <input
+          placeholder="password"
+          onChange={handleChange}
+          type="password"
+          name="password"
+          value={register.password}
+        ></input>
 
-        e.persist()
-        setRegister({ ...register, [e.target.name]: e.target.value })
+        <label>Email:</label>
+        <input
+          placeholder="email"
+          onChange={handleChange}
+          type="text"
+          name="email"
+          value={register.email}
+        ></input>
 
-        yup
-            .reach(registerSchema, e.target.name)
-            .validate(e.target.value)
-            .then(valid => {
+        <button disabled={!buttonEnabled} type="submit">
+          Register
+        </button>
 
-                setRegisterFormErrors({...registerFormErrors, [e.target.name]:''})
+        <p>{registerFormErrors.username}</p>
+        <p>{registerFormErrors.password}</p>
+        <p>{registerFormErrors.email}</p>
 
-            })
-            .catch(err => {
+        {register.isFetching && "Loading register page..."}
+      </form>
 
-                setRegisterFormErrors({...registerFormErrors, [e.target.name]: err.errors[0]})
-
-            })
-
-    }
-
-    const handleSubmit = (e) => {
-
-        e.preventDefault();
-
-        axios.post('', register)
-            .then(res => {
-
-                props.history.push('/')
-                console.log('Register data is returning', res)
-
-            })
-            .catch(err => {
-
-                console.log('Register data is not returning', err)
-
-            })
-
-    }
-
-    return (
-
-        <div className='register'>
-
-            <h1>Register</h1>
-
-                <form onSubmit={handleSubmit}>
-                    
-                    <label>Username:</label><input placeholder='username' onChange={handleChange} type='text' name='username' value={register.username}></input>
-
-                    <label>Password:</label><input placeholder='password' onChange={handleChange} type='password' name='password' value={register.password}></input>
-
-                    <label>Email:</label><input placeholder='email' onChange={handleChange} type='text' name='email' value={register.email}></input>
-
-                    <button disabled={!buttonEnabled} type='submit'>Register</button>
-
-                    <p>{registerFormErrors.username}</p>
-                    <p>{registerFormErrors.password}</p>
-                    <p>{registerFormErrors.email}</p>
-                
-
-                    {register.isFetching && 'Loading register page...'}
-
-                </form>
-
-                {/* <div>Already have an account? <Link to=''>Login here</Link></div> */}
-
-        </div>
-
-
-    )
-
-
-
+      {/* <div>Already have an account? <Link to=''>Login here</Link></div> */}
+    </div>
+  );
 }
 export default Register;
